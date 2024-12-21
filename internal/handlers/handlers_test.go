6252c1c81c2e4499ddd28d1a915ee24e34311354
solverAndimprover <son.21vo@gmail.com>
2024-12-21 22:37:29 +0300

    part of tests

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,25 @@
+package handlers
+
+//import (
+//	//"bufio"
+//	//"bytes"
+//	//"net/http"
+//	//"net/http/httptest"
+//	"testing"
+//)
+//
+//func TestCalculateHandler(t *testing.T) {
+//	testCases := []struct {
+//		request  string
+//		expected string
+//	}{
+//		{`{"expression": "(48-67+220)/5"}`, `{"result":40.2}`},
+//		{`{"expression": "5*5"}`, `{"result":25}`},
+//		{`{"expression": "2+3"}`, `{"result":5}`},
+//		{`{"expression": "5/2"}`, `{"result":2.5}`},
+//		{`{"expression": "555-444"}`, `{"result":111}`},
+//		{`{"expression": "1+2+3-4+5*8"}`, `{"result":42}`},
+//		{`{"expression": "(5-2*2.5)*111"}`, `{"result":0}`},
+//		{`{"expression": "(48+677)/5"}`, `{"result":145}`},
+//	}
+//	return
